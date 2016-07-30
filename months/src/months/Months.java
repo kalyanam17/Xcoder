68175e494db8c9debfc3a6b50950ec9c66133545
krishna kalyanam <kalyanamkrish@gmail.com>
2016-07-30 12:32:32 -0400

    commit

---
+++ months/src/months/Months.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package months;
+
+import java.util.Scanner;
+
+/**
+ * @author Krishna
+ *
+ */
+public class Months {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);//Scanner
+		System.out.println("Enter Month:");//intial Output
+		int month = sc.nextInt();
+		//Declare months
+		if (month<0||month>12) {
+			System.out.println("invalid Month");
+		}  else if (month==1) {
+			System.out.println("January");
+		}
+		else if (month == 2) {
+			System.out.println("Febuary");
+		}
+		else if (month==3) {
+			System.out.println("March");
+		}
+		else if (month==4) {
+			System.out.println("April");
+		}
+		else if (month==5) {
+			System.out.println("May");
+		}
+		else if (month==6) {
+			System.out.println("June");
+		}
+		else if (month==7) {
+			System.out.println("July");
+		}
+		else if (month==8) {
+			System.out.println("August");
+		}
+		else if (month==9) {
+			System.out.println("September");
+		}
+		else if (month==10) {
+			System.out.println("October");
+		}
+		else if (month==11) {
+			System.out.println("November");
+		}
+		else if (month==12) {
+			System.out.println("December");
+		}
+		sc.close();//close scanner
+		}
+
+	}
+
+	
+
+
